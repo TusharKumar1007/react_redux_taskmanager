ad67883bbaa0477b4ea6cc8cecdb120314651b10
Tusharkumar-dev <githubtk1007@gmail.com>
2026-01-21 13:13:31 +0530

    added user-input:valiation style

--- src/app/layout/loginForm.jsx
+++ src/app/layout/loginForm.jsx
@@ -56,7 +56,7 @@ export default function LoginForm() {
                         <input
                             type="email"
                             placeholder="Email: joe@email.com"
-                            className="text-slate-200 h-10 w-full px-2 border-b-2 outline-0 transition-all placeholder:text-slate-300 focus-within:bg-[#464242] focus-within:border-b-yellow-400  "
+                            className="text-slate-200 h-10 w-full px-2 border-b-2 outline-0 transition-all placeholder:text-slate-300 focus-within:bg-[#464242] focus-within:border-b-yellow-400 user-invalid:bg-red-500/30 user-valid:bg-green-400/30"
                             spellCheck="false"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
